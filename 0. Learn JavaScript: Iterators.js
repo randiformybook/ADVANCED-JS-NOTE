855e1f4c9bec12b9bed7086219d8eb2f70b7397c
randiformybook <124506252+randiformybook@users.noreply.github.com>
2023-03-07 21:56:26 +0700

    Update 0. Learn JavaScript: Iterators.js

--- 0. Learn JavaScript: Iterators.js
+++ 0. Learn JavaScript: Iterators.js
@@ -1,48 +1,35 @@
 //Callback Functions
 
-
 const isEven = (n) => {
-    return n % 2 == 0;
+	return n % 2 == 0;
 	//nilai return disini berbentuk boolean,
-jika n%2 ==0, tu adalah nilai true nya
+	jika n%2 ==0
+	//tu adalah nilai true nya
   }
-	//isEven ialah Function Pertama,yaitu function boolean
+//isEven ialah Function Pertama,yaitu function boolean
  
-  let printMsg = (evenFunc, num) => {
+let printMsg = (evenFunc, num) => {
 	//evenFunc akan dijadikan wadah yang berisi 
-function isEven
-
-
-//masukan ke dalam variable baru dengan bentuk 
-dengan num sebagai argument (n) nya
-    const isNumEven = evenFunc(num);
-    console.log(`The number ${num} is an even number: 
-${isNumEven}.`);
+	function isEven
+	//masukan ke dalam variable baru dengan bentuk dengan num sebagai argument (n) nya
+    	const isNumEven = evenFunc(num);
+    	console.log(`The number ${num} is an even number: ${isNumEven}.`);
   }
- 
-  // Pass in isEven as the callback function
-  printMsg(isEven, 4);
+
+// Pass in isEven as the callback function
+printMsg(isEven, 4);
 //maksud dari running function printMsg ialah : 
 printMsg(boolean, 4);
+// Prints: The number 4 is an even number: True.
 
-
-  // Prints: The number 4 is an even number: True.
-
-—--------------------------------------------
-	//Bentuk ketika di running seakan menjadi:
-	let printMsg = (isEven(4), 4) => {
-
-
-    const isNumEven = evenFunc(4);
-    console.log(`The number ${4} is an even number: 
-${apakah isEven(4%2 == 0)}.`);
-  }
-
-—----------------------------------------------------------------------------------------------------------------------------
+//—--------------------------------------------
+//Bentuk ketika di running seakan menjadi:
+let printMsg = (isEven(4), 4) => {
+	const isNumEven = evenFunc(4);
+	console.log(`The number ${4} is an even number: ${isEven(4%2 == 0)}`);
+	}
 
 //Functions Assigned to Variables
-
-
 let plusFive = (number) => {
   return number + 5;  
 };
